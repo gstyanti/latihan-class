6231b5efa243ebf2338bc4232e17fda361f338c8
gstyanti <101311934+gstyanti@users.noreply.github.com>
2022-04-12 21:08:37 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClass
+{
+    class Program
+    {
+        public void Run ()
+        {
+            System.DateTime currentTime = System.DateTime.Now;
+            Time time1 = new Time(currentTime);
+            time1.DisplayCurrentTime();
+            Time time2 = new Time(2003, 2, 1, 09, 23, 30);
+            time2.DisplayCurrentTime();
+        }
+
+        
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClass
+{
+   public class Time 
+    {
+        //public class Time
+         int year;
+        int month;
+        int date;
+         int hour;
+         int minute;
+         int second;
+
+        //public accessor methods
+        public void 
+            DisplayCurrentTimw()
+        {
+            System.Console.WriteLine("{0}/{1}/{2} {3}:{4}:{5}", month, date, year, hour, minute, second);
+        }
+
+        //constructor
+        public Time (System.DateTime dt)
+        {
+            year = dt.Year;
+            month = dt.Month;
+            date = dt.Day;
+            hour = dt.Hour;
+            minute = dt.Minute;
+            second = dt.Second;
+        }
+        
+        
+        //constructor
+        public Time (string strTime)
+        {
+            Console.WriteLine(strTime);
+        }
+        public Time (int year, int month, int date, int hour, int minute, int second)
+        {
+            this.year = year;
+            this.month = month;
+            this.date = date;
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+    }
+}
